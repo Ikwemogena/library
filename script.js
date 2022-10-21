c449923e7b9fdc52074073568f8a0a7ad381d1e3
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2022-10-21 23:17:52 +0100

    update

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-let library = []
 
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function(){
-        return(`title + author + ". " + pages + ", " + read`);
-    }
+
+const popupLogin = document.getElementById("myPopup");
+const showPop = document.getElementById("show-add");
+const mainContainer = document.getElementById("erase");
+
+
+showPop.addEventListener("click", () => {
+    popupLogin.classList.remove("hidden");
+    mainContainer.style.display = "none"
+})
+
+
+
+
+
+
+
+
+
+
+// let library = []
+
+// function Book(title, author, pages, read){
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+//     this.info = function(){
+//         return(`title + author + ". " + pages + ", " + read`);
+//     }
     
-}
+// }
 
-//function
 
-const  theHobbit = new Book (`"The hobbit, " by jrr` )
-console.log(theHobbit.info());
+// function myFunction() {
+//     var popup = document.getElementById("myPopup");
+//     popup.classList.toggle("show");
+//   }
